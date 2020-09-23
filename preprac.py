b5e51931743727c76c625d21e9748806653fab20
Stelele <giftmugweni@gmail.com>
2020-09-23 02:28:32 +0100

    added code for toggling with switch

---
+++ preprac.py
@@ -0,0 +1,33 @@
+import RPi.GPIO as GPIO
+import time
+import sys
+import traceback
+
+GPIO.setmode(GPIO.BOARD)
+
+def main():
+    LED_PIN = 18
+    SWITCH_PIN = 16
+
+    GPIO.setup(LED_PIN, GPIO.OUT)
+    GPIO.setup(SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    #GPIO.add_event_detect(SWITCH_PIN, GPIO.FALLING, bouncetime=200)
+
+    GPIO.wait_for_edge(SWITCH_PIN, GPIO.RISING, bouncetime=200)
+    GPIO.output(LED_PIN, not GPIO.input(LED_PIN))
+     
+
+if __name__ == "__main__":
+    try:
+        while True:
+            main()
+    except KeyboardInterrupt:
+        print("Exiting gracefully")
+        GPIO.cleanup()
+    except Exception as e:
+        print("Some error occured")
+        print("-"*60)
+        traceback.print_exc(file=sys.stdout)
+        print("-"*60)
+        GPIO.cleanup()
